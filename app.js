5cd5ef8138e2345fbff602164b420655448254e4
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-03-18 02:33:51 +0300

    18.03.2022-T02AM  Update

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGİNE
 app.set('view engine', 'ejs');
@@ -12,9 +20,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  /*res.sendFile(path.resolve(__dirname, 'views/index.ejs'));*/
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
 app.get('/about', (req, res) => {
   /*res.sendFile(path.resolve(__dirname, 'views/index.ejs'));*/
@@ -24,8 +34,8 @@ app.get('/add', (req, res) => {
   /*res.sendFile(path.resolve(__dirname, 'views/index.ejs'));*/
   res.render('add');
 });
-app.post('/photos', (req, res) => {
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  Photo.create(req.body);
   res.redirect('/');
 });
 
