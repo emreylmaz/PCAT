2cf8685022dcdd021b3021456319997e5bc745f9
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-03-22 00:20:27 +0300

    22.03.2022-T00:20AM  Update.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express');
 const ejs = require('ejs');
-const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const express = require('express');
 
 const app = express();
 
@@ -19,7 +20,9 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
+//Routes
 app.get('/', async (req, res) => {
   const photos = await Photo.find({});
   res.render('index', {
@@ -27,15 +30,12 @@ app.get('/', async (req, res) => {
   });
 });
 app.get('/photos/:id', async (req, res) => {
-  //console.log(req.params.id);
-  //res.render('about');
   const photo = await Photo.findById(req.params.id);
   res.render('photo', {
     photo,
   });
 });
 app.get('/about', (req, res) => {
-  /*res.sendFile(path.resolve(__dirname, 'views/index.ejs'));*/
   res.render('about');
 });
 app.get('/add', (req, res) => {
@@ -43,8 +43,29 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 app.post('/photos', async (req, res) => {
-  Photo.create(req.body);
-  res.redirect('/');
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadImage = req.files.image;
+  let uploadPath = uploadDir + '/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async (err) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/add');
+    } else {
+      const photo = new Photo({
+        title: req.body.title,
+        description: req.body.description,
+        image: '/uploads/' + uploadImage.name,
+      });
+      await photo.save();
+      res.redirect('/');
+    }
+  });
 });
 
 const port = 3000;
