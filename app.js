0fb8e0a12734f033030e2f613b17b8371115bf75
Emre Yılmaz <emreyilmaz.developer@gmail.com>
2022-03-22 03:17:10 +0300

    CRUD operations completed.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const ejs = require('ejs');
 const fs = require('fs');
 const Photo = require('./models/Photo');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const fileUpload = require('express-fileupload');
 const express = require('express');
 
@@ -21,6 +22,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //Routes
 app.get('/', async (req, res) => {
@@ -66,6 +72,41 @@ app.post('/photos', async (req, res) => {
       res.redirect('/');
     }
   });
+});
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', (req, res) => {
+  Photo.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, photo) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect(`/photos/${req.params.id}`);
+      }
+    }
+  );
+});
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/public' + photo.image;
+  /*fs.existsSync(deletedImage, (exists) => {
+    if (exists) {
+      console.log('File exists.');
+    } else {
+      fs.unlinkSync(deletedImage);
+    }
+  });*/
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndRemove(req.params.id);
+  res.redirect('/');
 });
 
 const port = 3000;
